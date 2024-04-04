ed1cc954f49c07ff3915d4f30c451165c5c95a5a
sarka137 <120708285+sarka137@users.noreply.github.com>
2024-04-04 20:48:37 +0200

    dokonceni

--- index.js
+++ index.js
@@ -17,25 +17,43 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Webová anketa'});
 });
 
-app.post('/submit', (req, res) => {
-    const newResponse = {
-      id: Date.now(),
-      timeStamp: new Date().toISOString(),
-      answers: req.body,
-    };
-    fs.readFile("responses.json", (err, data) => {
-      if (err) throw err;
-      let json = JSON.parse(data);
-      json.push(newResponse);
-      fs.writeFile("responses.json", JSON.stringify(json, null, 2), (err) => {
-        if (err) throw err;
-        console.log('Data byla úspěšně uložena');
-        res.redirect('/results');
-      });
-    
+app.post("/submit", (req, res) => {
+  // Zde budeme ukládat data z formuláře do souboru responses.json
+  const newResponse = {
+    id: Date.now(), // Jednoduchý způsob, jak generovat unikátní ID
+    timestamp: new Date().toISOString(),
+    answers: req.body, // Předpokládáme, že všechny odpovědi jsou ve formátu, který chceme uložit
+  };
+
+fs.readFile("responses.json", (err, data) => {
+  if (err) throw err;
+  let json = JSON.parse(data);
+  json.push(newResponse);
+
+  // Zápis aktualizovaných dat zpět do souboru
+  fs.writeFile("responses.json", JSON.stringify(json, null, 2), (err) => {
+    if (err) throw err;
+    console.log("Data byla úspěšně uložena.");
+    res.redirect("/results"); // Přesměrování na stránku s výsledky
+    });
+  });
+});
+
+
+/* Routa pro zobrazení výsledků ankety */
+app.get("/results", (req, res) => {
+// Zde bude načtení dat ze souboru responses.json a jejich předání do šablony
+fs.readFile('responses.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return res.status(500).send('Nastala chyba při čtení dat.');
+  }
+  const responses = JSON.parse(data);
+  res.render('results', { title: "Výsledky ankety", responses }); // Předání dat-odpovědí šabloně results.ejs
+});
 });
 
 app.get('/results', (req, res) => {
   res.render('results', { title: 'Výsledky ankety'});
-});})
+});
 
